f7a04bc868d54ca7e45cc5456ba5d6733aa3526e
Henry Rodriguez <128602858+hirodri@users.noreply.github.com>
2023-03-24 01:01:23 -0500

    Add files via upload

--- src/test/java/com/proyecto/demo/CrudSpringBootApplicationTests.java
+++ src/test/java/com/proyecto/demo/CrudSpringBootApplicationTests.java
@@ -3,11 +3,11 @@ package com.proyecto.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CrudSpringBootApplicationTests {
+//@SpringBootTest
+//class CrudSpringBootApplicationTests {
 
-	@Test
-void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
-}
+//}
